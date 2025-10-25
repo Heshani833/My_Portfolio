b775f6d6cca62dc82d79ebc0b83f09c2d7035997
Heshani833 <heshanishehana123@gmail.com>
2025-10-25 22:33:29 +0530

    created main.js

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+  strings: ["Creative Frontend Developer","Backend Developer", "Full Stack Developer", "MERN Stack Developer", "Web Designer", "AI/ML Enthusiast"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
